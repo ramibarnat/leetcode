6e1b121f0b743e767c81ddc56530870e019deb68
Rami Barnat <ramisouguir@gmail.com>
2024-09-03 19:21:42 -0400

    21, 4

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -3,17 +3,48 @@
 #
 # [4] Median of Two Sorted Arrays
 #
-from collections import List
+from typing import List
 # @lc code=start
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        pass
+        if len(nums1) < len(nums2):
+            smaller = nums1
+            bigger = nums2
+        else:
+            smaller = nums2
+            bigger = nums1
+
+        half = (len(bigger)+len(smaller)-1) >> 1
+        l,r = 0,len(smaller)-1
+        while True:
+            mid_s = (l+r) >> 1
+            mid_b = half-mid_s-1
+            if mid_s >= 0 and mid_b != len(bigger)-1 and smaller[mid_s] > bigger[mid_b+1]:
+                r = mid_s - 1
+            elif mid_b >= 0 and mid_s != len(smaller)-1 and bigger[mid_b] > smaller[mid_s+1]:
+                r += mid_s
+            else:
+                if mid_s < 0: 
+                    if (len(bigger)+len(smaller)%2==0):
+                        return bigger[-1]
+                    return (bigger[-1]+smaller[0])/2
+                if mid_b < 0: 
+                    if (len(bigger)+len(smaller)%2==0):
+                        return smaller[-1]
+                    return (smaller[-1]+bigger[0])/2
+                left_max = max(bigger[mid_b],smaller[mid_s])
+                if (len(bigger)+len(smaller)) % 2 == 0:
+                    x = float('inf') if mid_b == len(bigger)-1 else bigger[mid_b+1]
+                    y = float('inf') if mid_s == len(smaller)-1 else smaller[mid_s+1]
+                    right_min = min(x,y)
+                    return (right_min+left_max) / 2
+                return left_max
+
+                
+
 # @lc code=end
 
-[1,3,4,6,8,10]
-[5,9,16,20]
-
-[1,3,4,5,6, | 8,9,10,16,20]
-
-Ans: 7
-
+a=[1,2]
+b=[3,4]
+obj = Solution()
+print(obj.findMedianSortedArrays(a,b))
